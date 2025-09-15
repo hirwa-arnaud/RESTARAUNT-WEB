d1a9bef1e61be6a85d841a17ffd52ac11f36eed6
Hirwa Arrnaud <hirwatrevor8@gmail.com>
2025-09-15 09:31:57 +0200

    added interactivity

--- src/components/Drink.tsx
+++ src/components/Drink.tsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
 
-function Drink () {
-    return(
-        <div className="drink">
-            <div className="cok"><img src="wine.png"/></div>
-             <div className="cok">
-<h1>Wine</h1>
-<h1 className="cock">Cocktails</h1>  
-<h1>Beer</h1>
+function Drink() {
+  const images = ["wine.png", "wine.webp", "adrian2.png"];
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-             </div>
-        </div>
-    )
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+  return (
+    <div className="drink">
+      <div className="coc">
+        <img className="img" src={images[currentIndex]} alt="drink" />
+        <img src="right.png" alt="next" onClick={handleNext} />
+        <img src="left.png" alt="previous" onClick={handlePrev} />
+      </div>
+
+      <div className="cok">
+        <h1 className={currentIndex === 0 ? "cock" : ""}>Wine</h1>
+        <h1 className={currentIndex === 1 ? "cock" : ""}>Cocktails</h1>
+        <h1 className={currentIndex === 2 ? "cock" : ""}>Beer</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Drink;
